fef0c1eabcdf24b92981c79a04adfa5bcd4ab2e6
Poojan Ghetiya <poojanghetiya29@gmail.com>
2025-02-01 15:21:44 +0530

    Removed unnecessary files

--- tasks/permissions.py
+++
@@ -1,9 +0,0 @@
-# from rest_framework import permissions
-
-# class IsManagerOrAdmin(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return request.user.role in ['MANAGER', 'ADMIN']
-    
-# class IsTaskAssigneeOrAdmin(permissions.BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         return obj.assigned_to == request.user or request.user.role == 'ADMIN'
